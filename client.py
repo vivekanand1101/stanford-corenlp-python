03fa74f49501e3cd63d3de7171b49eef7db27e06
Hiroyoshi Komatsu <hiroyoshi.komat@gmail.com>
2013-04-25 16:26:19 +0900

    Using jsonrpclib

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 import json
-from jsonrpc import ServerProxy, JsonRpc20, TransportTcpIp
+# from jsonrpc import ServerProxy, JsonRpc20, TransportTcpIp
+import jsonrpclib
 from pprint import pprint
 
+
 class StanfordNLP:
-    def __init__(self):
-        self.server = ServerProxy(JsonRpc20(),
-                                  TransportTcpIp(addr=("127.0.0.1", 8080)))
-    
+    def __init__(self, port_number=8080):
+        self.server = jsonrpclib.Server("http://localhost:%d" % port_number)
+
     def parse(self, text):
         return json.loads(self.server.parse(text))
 
