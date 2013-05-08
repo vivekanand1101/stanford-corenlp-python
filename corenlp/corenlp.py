90d9988a7b026657921f604d9e39aa7d60696a5e
Hiroyoshi Komatsu <hiroyoshi.komat@gmail.com>
2013-05-08 21:57:42 +0900

    Adjust timeout

--- corenlp/corenlp.py
+++ corenlp/corenlp.py
@@ -246,7 +246,7 @@ class StanfordCoreNLP(object):
         # the idea here is that you increase the timeout as a
         # function of the text's length.
         # max_expected_time = max(5.0, 3 + len(to_send) / 5.0)
-        max_expected_time = max(300.0, len(to_send) / 2)
+        max_expected_time = max(300.0, len(to_send) / 3.0)
 
         # repeated_input = self.corenlp.except("\n")  # confirm it
         t = self.corenlp.expect(["\nNLP> ", pexpect.TIMEOUT, pexpect.EOF],
