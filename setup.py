9c99ab0298c25cff6c0663b0f47631ab532b942e
PlasmaSheep <plasmasheep@gmail.com>
2014-05-10 17:50:16 -0700

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+PACKAGE = "corenlp"
+NAME = "wordseer-stanford-corenlp-python"
+DESCRIPTION = "A Stanford Core NLP wrapper (wordseer fork)"
+AUTHOR = "Hiroyoshi Komatsu, Dustin Smith, Aditi Muralidharan"
+AUTHOR_EMAIL = "aditi.shrikumar@gmail.com"
+URL = "https://github.com/silverasm/stanford-corenlp-python"
+VERSION = "3.3.0-0"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    packages=find_packages(),
+    package_data = {"": ["*.properties"],
+        "corenlp": ["*.properties"]},
+    install_requires=[
+        "pexpect >= 2.4",
+        "unidecode >= 0.04.12",
+        "xmltodict >= 0.4.6",
+    ],
+    classifiers=[
+        ("License :: OSI Approved :: GNU General Public License v2 or later "
+            "(GPLv2+)"),
+        "Programming Language :: Python",
+    ],
+)
