aec4b351c3e7d5ddbbedc2f72c43bb38a32fb708
PlasmaSheep <plasmasheep@gmail.com>
2014-08-02 13:33:16 -0700

    Removed debug code

--- corenlp/corenlp.py
+++ corenlp/corenlp.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-import pdb
+
 import json
 import optparse
 import os
@@ -171,7 +171,6 @@ def parse_parser_results(text):
             if not line.startswith("[Text="):
                 raise ParserError('Parse error. Could not find "[Text=" in: %s' % line)
             for s in WORD_PATTERN.findall(line):
-                pdb.set_trace()
                 sentence['words'].append(parse_bracketed(s))
             state = STATE_TREE
 
