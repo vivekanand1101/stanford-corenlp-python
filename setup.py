e1d7c5619bfc2ab09fdd6589c1bc50530dd50f52
PlasmaSheep <plasmasheep@gmail.com>
2014-08-13 11:04:58 -0700

    Now with pep 396!

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = "A Stanford Core NLP wrapper (wordseer fork)"
 AUTHOR = "Hiroyoshi Komatsu, Dustin Smith, Aditi Muralidharan"
 AUTHOR_EMAIL = "aditi.shrikumar@gmail.com"
 URL = "https://github.com/Wordseer/stanford-corenlp-python"
-VERSION = "3.3.8-0"
+VERSION = "3.3.8"
 INSTALLATION_REQS = ["unidecode >= 0.04.12", "xmltodict >= 0.4.6"]
 
 PEXPECT = "pexpect >= 2.4"
